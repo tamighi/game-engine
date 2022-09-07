3d2dddfda23fcc869404aa28ecaebf8da9ab0efa
Lysique <tamighi@student.s19.be>
2022-09-07 18:08:12 +0200

    push because freeze

--- Engine/src/Engine/Event/Event.hpp
+++ Engine/src/Engine/Event/Event.hpp
@@ -23,9 +23,17 @@ namespace Engine
 		EventCategoryMouseButton	= BIT(4)
 	};
 
+#define EVENT_CLASS_TYPE(type)	static EventType GetStaticType() { return EventType::##type; } \
+				virtual EventType GetEventType() const override { return GetStaticType(); } \
+				virtual const char* GetName() const override {return #type; }
+
 	class Event
 	{
-		virtual int GetCategoryFlags() = 0;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual std::string ToString() const { return getName(); };
 
 		inline bool IsInCategory(EventCategory& category)
 		{
